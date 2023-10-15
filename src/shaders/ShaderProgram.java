3f97c3ca72f551c2242cbfc84f6babf97c96335c
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-10-15 13:20:33 +0530

    Uniform variables

--- src/shaders/ShaderProgram.java
+++ src/shaders/ShaderProgram.java
@@ -2,6 +2,7 @@ package shaders;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Vector3f;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -19,9 +20,16 @@ public abstract class ShaderProgram {
         programID = GL20.glCreateProgram();
         GL20.glAttachShader(programID,vertexShaderID);
         GL20.glAttachShader(programID,fragmentShaderID);
+        bindAttributes();
         GL20.glLinkProgram(programID);
         GL20.glValidateProgram(programID);
+        getAllUniformLocations();
+    }
 
+    protected abstract void getAllUniformLocations();
+
+    protected int getUniformLocation(String uniformName){
+        return GL20.glGetUniformLocation(programID, uniformName);
     }
 
     public void start(){
@@ -46,6 +54,23 @@ public abstract class ShaderProgram {
     protected void bindAttributes(int attribute, String variableName){
         GL20.glBindAttribLocation(programID,attribute,variableName);
     }
+
+    protected void loadFloat(int location, float value){
+        GL20.glUniform1f(location,value);
+    }
+
+    protected void loadVector(int location, Vector3f vector){
+        GL20.glUniform3f(location,vector.x,vector.y,vector.z);
+    }
+
+    protected void loadBoolean(int location, boolean value){
+        float toLoad =0;
+        if (value){
+            toLoad=1;
+        }
+        GL20.glUniform1f(location,toLoad);
+    }
+
 
     private static int loadShader(String file, int type){
         StringBuilder shaderSource = new StringBuilder();
